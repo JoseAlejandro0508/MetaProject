71cf9fad8b7a8d57f75a5e097d4b89c81cf09787
alejandro20050830 <96395408+alejandro20050830@users.noreply.github.com>
2026-05-19 03:38:41 -0400

    UI 8.1

--- Meta-xi-Client-main/Meta-xi-Client-main/src/app/common/background/background.component.ts
+++ Meta-xi-Client-main/Meta-xi-Client-main/src/app/common/background/background.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { NavComponent } from '../../shared/nav/nav.component';
 import { ButtonsComponent } from '../../shared/buttons/buttons.component';
 import { TopNotificationComponent } from '../../shared/top-notification/top-notification.component';
@@ -7,8 +9,31 @@ import { TopNotificationComponent } from '../../shared/top-notification/top-noti
 @Component({
   selector: 'app-background',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, ButtonsComponent, TopNotificationComponent],
+  imports: [RouterOutlet, NavComponent, ButtonsComponent, TopNotificationComponent, CommonModule],
   templateUrl: './background.component.html',
   styleUrl: './background.component.scss',
 })
-export class BackgroundComponent {}
+export class BackgroundComponent implements OnInit {
+  hideNavAndButtons = false;
+
+  private readonly hideRoutes = [
+    'withdraw/nequi',
+    'withdraw/usdt-trc20',
+    'nequi/nequi',
+    'deposit/usdt-bep20',
+    'deposit/breb'
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        const url = event.urlAfterRedirects || event.url;
+        this.hideNavAndButtons = this.hideRoutes.some((route) =>
+          url.includes(route)
+        );
+      });
+  }
+}
